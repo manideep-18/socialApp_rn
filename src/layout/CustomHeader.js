877e3268164dd1e2791612de91e7dc8e34cfa8a8
manideep-18 <r8manideep@gmail.com>
2020-12-13 20:41:11 +0530

    customheader layout file created

---
+++ src/layout/CustomHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {StyleSheet,Text} from 'react-native'
+
+const CustomHeader=()=>{
+    return(
+        <>
+        <Text>Hello from CustomHeader</Text>
+        </>
+    )
+}
+
+export default CustomHeader
