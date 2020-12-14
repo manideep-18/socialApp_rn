aca6c1dcd8e78ec38d25cbd48404e6cf6993fdc5
manideep-18 <r8manideep@gmail.com>
2020-12-14 10:54:53 +0530

    store part code updated

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import rootReducer from './reducer'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store=createStore(rootReducer)
+const middleware=[thunk]
+
+const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(...applyMiddleware)))
 
 export default store
