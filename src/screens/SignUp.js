61b559f93d0adfae34c25b1098373a42e90800c1
manideep-18 <r8manideep@gmail.com>
2020-12-15 19:45:18 +0530

    initial state values implemented in signup screen

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,7 +1,37 @@
 import React from 'react'
-import {StyleSheet,Text} from 'react-native'
+import {StyleSheet,ScrollView,TouchableOpacity,View} from 'react-native'
+import {
+    Container,
+    Form,
+    Item,
+    Input,
+    Text,
+    Button,
+    Thumbnail,
+    Content
+} from 'native-base'
+import storage from '@react-native-firebase/storage'
+import ProgressBar from 'react-native-progress/Bar'
+import ImagePicker from 'react-native-image-picker'
+import propTypes from 'prop-types'
 
-const SignUp=()=>{
+
+import {signUp} from '../action/auth'
+import options from '../utils/Options'
+import { connect } from 'react-redux'
+import { useState } from 'react'
+
+const SignUp=({signUp})=>{
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const [instaUserName,setInstaUserName]=useState('')
+    const [country,setCountry]=useState('')
+    const [bio,setBio]=useState('')
+    const [image,setImage]=useState('https://www.gstatic.com/devrel-devsite/prod/vf7e3a995d426e05d42b78fc7d21a14329a91016dc065dc22c480cc8f443ef33e/firebase/images/touchicon-180.png')
+    const [imageUploading,setImageUploading]=useState(false)
+    const [uploadStatus,setUploadStatus]=useState(null)
+
     return(
         <>
         <Text>Hello from SignUp</Text>
@@ -9,4 +39,12 @@ const SignUp=()=>{
     )
 }
 
-export default SignUp
+const mapDispatchToProps={
+    signUp:(data)=>signUp(data)
+}
+
+SignUp.propTypes={
+    signUp:propTypes.func.isRequired
+}
+
+export default connect(null,mapDispatchToProps)(SignUp)
