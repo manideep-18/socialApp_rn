c695fd353ff41dde8d81987cbb0d997f1b538670
manideep-18 <r8manideep@gmail.com>
2020-12-18 23:03:49 +0530

    addpost code implemented without addpost function implementation

--- src/screens/AddPost.js
+++ src/screens/AddPost.js
@@ -1,12 +1,163 @@
-import React from 'react'
-import {StyleSheet,Text} from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, ScrollView, Image} from 'react-native';
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Text,
+  Button,
+  H3,
+  Textarea,
+  Icon,
+} from 'native-base';
+import Snackbar from 'react-native-snackbar'
+import ProgressBar from 'react-native-progress/Bar'
+import database from '@react-native-firebase/database'
+import storage from '@react-native-firebase/storage'
+import ImagePicker from 'react-native-image-picker'
+import options from '../utils/Options'
 
-const AddPost=()=>{
-    return(
-        <>
-        <Text>Hello from AddPost</Text>
-        </>
-    )
+import {connect} from 'react-redux'
+import propTypes from 'prop-types'
+import shortid from 'shortid'
+
+
+const AddPost=({navigation,userState})=>{
+    const [location,setLocation]=useState('')
+    const [description,setDescription]=useState('')
+    const [image,setImage]=useState('')
+
+    const [imageUploading,setImageUploading]=useState(false)
+    const [uploadStatus,setUploadStatus]=useState(null)
+
+    const chooseImage=async()=>{
+        ImagePicker.showImagePicker(options,(respose)=>{
+            console.log("Respose =",respose)
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+              } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+              } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+              } else {
+               console.log(respose)
+               uploadImage(respose)
+              }
+            
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+        })
+    }
+
+    const uploadImage=async(response)=>{
+        setImageUploading(true)
+        const reference=storage().ref(response.fileName)
+
+        const task=reference.putFile(response.path)
+        task.on('state_changed',(takeSnapshot)=>{
+            const percentage=((takeSnapshot.bytesTransferred/takeSnapshot.totalBytes))*1000
+
+            setUploadStatus(percentage)
+        })
+
+        task.then(async()=>{
+            const url=await reference.getDownloadURL()
+
+            setImage(url)
+            setImageUploading(false)
+        })
+    }
+
+    const addPost=async ()=>{
+
+    }
+
+    return (
+        <Container style={styles.container}>
+            <Content padder>
+                <ScrollView contentContainerStyle={{flexGrow: 1}}>
+                    {image!=='' && (
+                        <Image
+                            source={{uri: image}}
+                            style={styles.image}
+                            resizeMode="center"
+                        />
+                    )}
+                    <Form>
+                    <Item regular style={styles.formItem}>
+              <Input
+                placeholder="location"
+                value={location}
+                style={{color: '#eee'}}
+                onChangeText={(text) => setLocation(text)}
+              />
+            </Item>
+            {imageUploading ? (
+              <ProgressBar progress={uploadStatus} style={styles.progress} />
+            ) : (
+              <Button
+                regular
+                bordered
+                block
+                iconLeft
+                info
+                style={styles.formItem}
+                onPress={chooseImage}>
+                <Icon
+                  name="md-image-outline"
+                  type="Ionicons"
+                  style={styles.icon}
+                />
+                <Text
+                  style={{
+                    color: '#fdcb9e',
+                  }}>
+                  Choose Image
+                </Text>
+              </Button>
+            )}
+            <Item regular style={styles.formItem}>
+              <Textarea
+                rowSpan={5}
+                placeholder="Some description..."
+                value={description}
+                style={{color: '#eee'}}
+                onChangeText={(text) => setDescription(text)}
+              />
+            </Item>
+            <Button regular block onPress={addPost}>
+              <Text>Add Post</Text>
+            </Button>
+                    </Form>
+                </ScrollView>
+            </Content>
+        </Container>
+        )
 }
 
-export default AddPost
+const mapStateToProps=(state)=>({
+    userState:state.auth.user
+})
+
+// AddPost.propTypes={
+//     userState:propTypes.object.isRequired
+// }
+
+export default connect(mapStateToProps)(AddPost)
+
+ const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1b262c',
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  formItem: {
+    marginBottom: 20,
+  },
+  icon: {fontSize: 20, color: '#fdcb9e'},
+  image: {width: null, height: 150, marginVertical: 15},
+  progress: {width: null, marginBottom: 20},
+});
