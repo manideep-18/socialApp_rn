5f38b3083ad124eb0e7a83c2b9fb9a43ab57d3f7
manideep-18 <r8manideep@gmail.com>
2020-12-18 21:38:29 +0530

    signin screen implemented

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const App=({authState})=>{
         </>
       ):(
         <>
-       <Stack.Screen name="SignIn" component={SignUp}/>
+       <Stack.Screen name="SignIn" component={SignIn}/>
        <Stack.Screen name="SignUp" component={SignUp}/>
         </>
       )}
